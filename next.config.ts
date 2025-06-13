967f8f02d1b63d7320feae0047481de639ddcb7b
Gonalex <angelosgamedev@gmail.com>
2025-06-14 00:40:03 +0300

    Add domain redirect configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'angelosportfolio.info',
+          },
+        ],
+        destination: 'https://www.angelosportfolio.info',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
